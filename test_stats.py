cb64ebd39ac658a09fe0066f951c5fd017163d9e
Ken Drinkwater <kendrinkwater@gmail.com>
2015-08-27 13:40:49 -0500

    jdsfaj

--- test_stats.py
+++ test_stats.py
@@ -1,10 +1,23 @@
 from stats import mean
+from nose.tools import assert_equal
+
+
 
 def test_mean():
-        assert(mean([2,4]) == 3)
-test_mean() 
+        assert_equal(mean([2,4]), 3)
+#test_mean() 
 
 def test_float_mean():
         assert(mean([2,3]) == 2.5)
 
-test_float_mean()
+#test_float_mean()
+
+def test_negs():
+	assert(mean([-2,-4]) == -3)
+#test_negs()
+
+def test_float():
+	assert(mean([2.0,4.0]) == 3)
+#test_float()
+
+
